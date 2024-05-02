b2170eaaac1d7c91c025100a9f690d30c7cb6ee7
Multyjog <simon.kotov.web@gmail.com>
2024-05-02 13:11:15 +0700

    Handle errors

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,11 +2,13 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
 import HomePage from "./pages/HomePage";
 import GameDetails from "./pages/GameDetailsPage";
+import ErrorPage from "./pages/ErrorPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
       { path: "games/:id", element: <GameDetails /> },
